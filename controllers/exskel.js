ffb63cbb2cb271de584731fe2ff26e1ae758b2bd
Michel Gutierrez <michel.gutierrez@gmail.com>
2016-01-09 02:42:30 +0100

    updated analytics code to google recommendation

--- controllers/exskel.js
+++ controllers/exskel.js
@@ -247,6 +247,15 @@ function GoogleAnalytics() {
 	if(config.gaTrackingCode) {
 		var parts = [];
 		parts.push("<script>");
+		parts.push("  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
+		parts.push("  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
+		parts.push("m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
+		parts.push("})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
+		parts.push("ga('create', '"+ config.gaTrackingCode +"', '"+ this.req.hostname +"');");
+		parts.push("ga('send', 'pageview','" + (this.gaPath || this.req.path) + "');");
+		parts.push("</script>");
+		/*
+		parts.push("<script>");
 		parts.push("window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;");
 		parts.push("ga('create', '"+ config.gaTrackingCode +"', '"+this.req.hostname+"');");
 		parts.push("ga('send', 'pageview','" + (this.gaPath || this.req.path) + "');");
@@ -256,6 +265,7 @@ function GoogleAnalytics() {
 			});
 		parts.push("</script>");
 		parts.push("<script async src='//www.google-analytics.com/analytics.js'></script>");
+		*/
 		return parts.join("\n");
 	}
 	return "";
